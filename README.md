9e21500d21cac69de1a2c60dd0c2e027772f1fd6
Mikael Midtgaard <mikaelmidt@gmail.com>
2017-01-19 23:05:55 +0100

    Added support for all fund types

--- README.md
+++ README.md
@@ -12,3 +12,28 @@ Run
 ```
 npm start
 ```
+
+Add environment variables for fund type and number of shares.
+```
+SHARES_COUNT=51 FUND_TYPE=OPPORTUNITY npm start
+```
+
+`SHARES_COUNT` is a number with default value of `1`.
+
+`FUND_TYPE` is one of `DEFENSIVE`, `MODERATE_SHORT`, `MODERATE`, `BALANCED`, `PROGRESSIVE`, `OPPORTUNITY` with a default value of `BALANCED`.
+
+## Code quality
+Testing
+```
+npm test
+```
+
+Test coverage
+```
+npm run test:coverage
+```
+
+Linting
+```
+npm run lint
+```
--- index.js
+++ index.js
@@ -2,17 +2,23 @@
 
 'use strict';
 
-const valuationApi = require('./lib/api/valuation');
-const fundSquareValuationParser = require('./lib/parse/fundSquareValuation');
+const valuationUtils = require('./lib/utils/valuation');
 const messageUtils = require('./lib/utils/message');
+const fundTypeConstants = require('./lib/constant/fundType');
 
 const sharesCount = process.env.SHARES_COUNT || 1;
+const fundType = process.env.FUND_TYPE || fundTypeConstants.balanced;
 let hasWaited = false;
 
 function checkValuation() {
-  valuationApi.getJuneBalancedSite()
-    .then(site => fundSquareValuationParser.parseFundSquareValuationSite(site))
+  valuationUtils.getValuation(fundType)
     .then((valuation) => {
+      if (!valuation) {
+        console.log('Invalid fund type supplied.');
+        console.log('Valid types are:');
+        console.log('DEFENSIVE, MODERATE_SHORT, MODERATE, BALANCED, PROGRESSIVE, OPPORTUNITY');
+        return;
+      }
       if (valuation.date.getDay() !== new Date().getDay()) {
         console.log('Today\'s valuation is not yet up, checking again in 5 minutes.');
         hasWaited = true;
@@ -20,7 +26,7 @@ function checkValuation() {
       } else {
         console.log('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n');
         messageUtils.printJuneLogo();
-        console.log('\n\n');
+        console.log('\n', fundType, '\n');
         if (hasWaited) {
           console.log(new Date());
         }
@@ -28,9 +34,11 @@ function checkValuation() {
         console.log('New:         ', valuation.value, ' ', valuation.value * sharesCount);
         console.log('change:      ', valuation.isPositive ? '+' : '-', valuation.change);
         console.log('Old:         ', valuation.previousValue, ' ', valuation.previousValue * sharesCount);
-        console.log('value change:', (valuation.value - valuation.previousValue) * sharesCount);
-        console.log('\n');
+        console.log('value change:', (valuation.value - valuation.previousValue) * sharesCount, '\n');
       }
+    })
+    .catch((e) => {
+      console.log('Something went wrong.', e.message);
     });
 }
 
